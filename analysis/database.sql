6c0d465588a7061627846719f2db41cf4b0caf9d
Henrique <hss@outlook.com.br>
2023-06-22 22:34:34 -0300

    Database optimization

--- analysis/database.sql
+++ analysis/database.sql
@@ -1,123 +1,56 @@
-CREATE TABLE `notebook` (
-  `id` int UNIQUE PRIMARY KEY NOT NULL,
-  `name` vvarchar2varchar2archar2varchar2(50) NOT NULL,
-  `confusion_matrix` blob NOT NULL,
-  `loss` decimal(20ion_mat,15) NOT NULL,
-  `accuracy` decimal(20,15) NOT NULL,
-  `auc` decimal(20,15) NOT NULL,
-  `recall` decimal(20,15) NOT NULL,
-  `precision` decimal(20,15) NOT NULL,
-  `false_positives` int NOT NULL,
-  `false_negatives` int NOT NULL,
-  `true_negatives` int NOT NULL,
-  `true_positives` int NOT NULL,
-  `val_loss` decimal(20,15) NOT NULL,
-  `val_accuracy` decimal(20,15) NOT NULL,
-  `val_auc` decimal(20,15) NOT NULL,
-  `val_recall` decimal(20,15) NOT NULL,
-  `val_precision` decimal(20,15) NOT NULL,
-  `val_false_positives` int NOT NULL,
-  `val_false_negatives` int NOT NULL,
-  `val_true_negatives` int NOT NULL,
-  `val_true_positives` int NOT NULL,
-  `configuration_id` int NOT NULL
+CREATE TABLE IF NOT EXISTS notebook (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT NOT NULL UNIQUE,
+  architecture TEXT CHECK(architecture IN ('ConvNeXt', 'ResNetV2', 'Xception')) NOT NULL,
+  confusion_matrix BLOB NOT NULL,
+  loss DECIMAL(20,15) NOT NULL,
+  accuracy DECIMAL(20,15) NOT NULL,
+  auc DECIMAL(20,15) NOT NULL,
+  recall DECIMAL(20,15) NOT NULL,
+  precision DECIMAL(20,15) NOT NULL,
+  false_positives INTEGER NOT NULL,
+  false_negatives INTEGER NOT NULL,
+  true_negatives INTEGER NOT NULL,
+  true_positives INTEGER NOT NULL,
+  val_loss DECIMAL(20,15) NOT NULL,
+  val_accuracy DECIMAL(20,15) NOT NULL,
+  val_auc DECIMAL(20,15) NOT NULL,
+  val_recall DECIMAL(20,15) NOT NULL,
+  val_precision DECIMAL(20,15) NOT NULL,
+  val_false_positives INTEGER NOT NULL,
+  val_false_negatives INTEGER NOT NULL,
+  val_true_negatives INTEGER NOT NULL,
+  val_true_positives INTEGER NOT NULL,
+  loss_epochs BLOB NOT NULL,
+  accuracy_epochs BLOB NOT NULL,
+  auc_epochs BLOB NOT NULL,
+  recall_epochs BLOB NOT NULL,
+  precision_epochs BLOB NOT NULL,
+  configuration_id INTEGER NOT NULL,
+  FOREIGN KEY (configuration_id) REFERENCES configuration (id)
 );
 
-CREATE TABLE `configuration` (
-  `id` int UNIQUE PRIMARY KEY NOT NULL,
-  `architecture` ENUM ('ConvNeXt', 'ResNetV2', 'Xception') NOT NULL,
-  `transfer_learning` boolean NOT NULL,
-  `freeze_layers` boolean NOT NULL,
-  `balance_classes` boolean NOT NULL,
-  `segmentation` boolean NOT NULL,
-  `data_augmentation` boolean NOT NULL,
-  `normalization` ENUM ('minMaxScaler', 'StandardScaler') NOT NULL,
-  `custom_optimizer` boolean NOT NULL,
-  `callback` boolean NOT NULL
+CREATE TABLE IF NOT EXISTS configuration (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  transfer_learning INTEGER NOT NULL,
+  freeze_layers INTEGER NOT NULL,
+  balance_classes INTEGER NOT NULL,
+  segmentation INTEGER NOT NULL,
+  data_augmentation INTEGER NOT NULL,
+  normalization TEXT CHECK(normalization IN ('minMaxScaler', 'StandardScaler')),
+  custom_optimizer INTEGER NOT NULL,
+  callback INTEGER NOT NULL
 );
 
-CREATE TABLE `bowens_disease` (
-  `id` int UNIQUE PRIMARY KEY NOT NULL,
-  `confusion_matrix` blob NOT NULL,
-  `precision` decimal(20,15) NOT NULL,
-  `f1_score` decimal(20,15) NOT NULL,
-  `roc_auc` decimal(20,15) NOT NULL,
-  `recall` decimal(20,15) NOT NULL,
-  `specificity` decimal(20,15) NOT NULL
+CREATE TABLE IF NOT EXISTS metrics (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  type TEXT CHECK(type IN ('Basal_cell_carcinoma', 'Benign_keratosis', 'Bowens_disease', 'Dermatofibroma', 'Melanocytic_nevi', 'Melanoma', 'Vascular_lesion')) NOT NULL,
+  confusion_matrix BLOB NOT NULL,
+  precision DECIMAL(20,15) NOT NULL,
+  f1_score DECIMAL(20,15) NOT NULL,
+  roc_auc DECIMAL(20,15) NOT NULL,
+  recall DECIMAL(20,15) NOT NULL,
+  specificity DECIMAL(20,15) NOT NULL,
+  notebook_id INTEGER NOT NULL,
+  FOREIGN KEY (notebook_id) REFERENCES notebook (id)
 );
-
-CREATE TABLE `basal_cell_carcinoma` (
-  `id` int UNIQUE PRIMARY KEY NOT NULL,
-  `confusion_matrix` blob NOT NULL,
-  `precision` decimal(20,15) NOT NULL,
-  `f1_score` decimal(20,15) NOT NULL,
-  `roc_auc` decimal(20,15) NOT NULL,
-  `recall` decimal(20,15) NOT NULL,
-  `specificity` decimal(20,15) NOT NULL
-);
-
-CREATE TABLE `benign_keratosis` (
-  `id` int UNIQUE PRIMARY KEY NOT NULL,
-  `confusion_matrix` blob NOT NULL,
-  `precision` decimal(20,15) NOT NULL,
-  `f1_score` decimal(20,15) NOT NULL,
-  `roc_auc` decimal(20,15) NOT NULL,
-  `recall` decimal(20,15) NOT NULL,
-  `specificity` decimal(20,15) NOT NULL
-);
-
-CREATE TABLE `dermatofibroma` (
-  `id` int UNIQUE PRIMARY KEY NOT NULL,
-  `confusion_matrix` blob NOT NULL,
-  `precision` decimal(20,15) NOT NULL,
-  `f1_score` decimal(20,15) NOT NULL,
-  `roc_auc` decimal(20,15) NOT NULL,
-  `recall` decimal(20,15) NOT NULL,
-  `specificity` decimal(20,15) NOT NULL
-);
-
-CREATE TABLE `vascular_lesion` (
-  `id` int UNIQUE PRIMARY KEY NOT NULL,
-  `confusion_matrix` blob NOT NULL,
-  `precision` decimal(20,15) NOT NULL,
-  `f1_score` decimal(20,15) NOT NULL,
-  `roc_auc` decimal(20,15) NOT NULL,
-  `recall` decimal(20,15) NOT NULL,
-  `specificity` decimal(20,15) NOT NULL
-);
-
-CREATE TABLE `melanoma` (
-  `id` int UNIQUE PRIMARY KEY NOT NULL,
-  `confusion_matrix` blob NOT NULL,
-  `precision` decimal(20,15) NOT NULL,
-  `f1_score` decimal(20,15) NOT NULL,
-  `roc_auc` decimal(20,15) NOT NULL,
-  `recall` decimal(20,15) NOT NULL,
-  `specificity` decimal(20,15) NOT NULL
-);
-
-CREATE TABLE `melanocytic_nevi` (
-  `id` int UNIQUE PRIMARY KEY NOT NULL,
-  `confusion_matrix` blob NOT NULL,
-  `precision` decimal(20,15) NOT NULL,
-  `f1_score` decimal(20,15) NOT NULL,
-  `roc_auc` decimal(20,15) NOT NULL,
-  `recall` decimal(20,15) NOT NULL,
-  `specificity` decimal(20,15) NOT NULL
-);
-
-ALTER TABLE `bowens_disease` ADD FOREIGN KEY (`id`) REFERENCES `notebook` (`id`);
-
-ALTER TABLE `basal_cell_carcinoma` ADD FOREIGN KEY (`id`) REFERENCES `notebook` (`id`);
-
-ALTER TABLE `benign_keratosis` ADD FOREIGN KEY (`id`) REFERENCES `notebook` (`id`);
-
-ALTER TABLE `dermatofibroma` ADD FOREIGN KEY (`id`) REFERENCES `notebook` (`id`);
-
-ALTER TABLE `vascular_lesion` ADD FOREIGN KEY (`id`) REFERENCES `notebook` (`id`);
-
-ALTER TABLE `melanoma` ADD FOREIGN KEY (`id`) REFERENCES `notebook` (`id`);
-
-ALTER TABLE `melanocytic_nevi` ADD FOREIGN KEY (`id`) REFERENCES `notebook` (`id`);
-
-ALTER TABLE `notebook` ADD FOREIGN KEY (`configuration_id`) REFERENCES `configuration` (`id`);
